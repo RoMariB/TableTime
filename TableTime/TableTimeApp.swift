12ee0af2151683e09e559156fe9244f3117cedd5
Maria Romanchuk <mashulya_r_88@mail.ru>
2023-02-03 18:55:37 +0700

    Initial Commit

---
+++ TableTime/TableTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableTimeApp.swift
+//  TableTime
+//
+//  Created by Мария Романчук on 03.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct TableTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
